ee6fdb307fc7944184787a91efc0381bc401ed04
vrym2 <vrym2@student.le.ac.uk>
2023-01-06 23:52:05 +0000

    added pre-commit-config.yaml

--- tests/data/test_download_data.py
+++ tests/data/test_download_data.py
@@ -1,6 +1,7 @@
 from ukpn.data import DownloadMetadata
 
+
 def test_download_metadata():
-    cantubry_api_url = 'https://ukpowernetworks.opendatasoft.com/api/records/1.0/search/?dataset=embedded-capacity-register&q=&facet=grid_supply_point&facet=licence_area&facet=energy_conversion_technology_1&facet=flexible_connection_yes_no&facet=connection_status&facet=primary_resource_type_group&refine.grid_supply_point=CANTERBURY+NORTH&refine.energy_conversion_technology_1=Photovoltaic'
+    cantubry_api_url = "https://ukpowernetworks.opendatasoft.com/api/records/1.0/search/?dataset=embedded-capacity-register&q=&facet=grid_supply_point&facet=licence_area&facet=energy_conversion_technology_1&facet=flexible_connection_yes_no&facet=connection_status&facet=primary_resource_type_group&refine.grid_supply_point=CANTERBURY+NORTH&refine.energy_conversion_technology_1=Photovoltaic"
     download = DownloadMetadata(api_url=cantubry_api_url)
-    download.get_data()    
+    download.get_data()
--- ukpn/data/download_data.py
+++ ukpn/data/download_data.py
@@ -1,36 +1,46 @@
-import requests
+"""This class is ued to retrieve data through API calls"""
 import json
-from pprint import pprint
 import logging
+from pprint import pprint
+
+import requests
 
 logger = logging.getLogger(__name__)
 
-class DownloadMetadata():
 
-    def __init__(
-        self,
-        api_url: str,
-        print_data:bool = False) -> None:
+class DownloadMetadata:
+    """Downloading the metadata using api requests"""
+
+    def __init__(self, api_url: str, print_data: bool = False) -> None:
+        """
+        This function retrievs metadata through api calls
+
+        Args:
+            api_url: The api url link that emiits json format data
+            print_data: Optional to choose printing the data
+        """
 
         self.api_url = api_url
         self.print_data = print_data
-        
+
         self.response_api = requests.get(self.api_url)
         while True:
             if self.response_api == 200:
-                logger.info(f"The api resposne is successful")
+                logger.info(f"The api resposne {self.response_api} is successful")
             else:
-                logger.warning(f"The api resposne is unsuccessul, please insert correct url")
+                logger.warning(f"The api resposne {self.response_api} is unsuccessul")
+                logger.info(f"Please enter the correct {'url'}")
                 break
-    
-    def get_data(self)-> None:
-             
+
+    def get_data(self) -> None:
+        """Loads the data into the JSON format"""
+
         # Get the data from the resposne
         self.raw_data = self.response_api.text
 
         # Parse the data into json format
         data_json = json.loads(self.raw_data)
-        data_first_record = data_json['records'][0]
+        data_first_record = data_json["records"][0]
 
         if self.print_data:
             pprint(data_first_record)
