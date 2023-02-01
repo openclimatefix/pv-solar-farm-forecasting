86bda2586141d119bf272ad4477b1158de88bb04
vrym2 <vrym2@student.le.ac.uk>
2023-02-01 11:17:11 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+""" Usual setup file for package """
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+install_requires = (this_directory / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="pv-solar-farm-forecasting",
+    description="Datapipes built for use in Open Climate Fix's solar farm forecasting ",
+    author_email="info@openclimatefix.org",
+    company="Open Climate Fix Ltd",
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    packages=find_packages(),
+)
