0106e0397d4e7ba30a4323fdb92c63286c733da7
vrym2 <vardhan609@gmail.com>
2023-02-20 16:24:52 +0000

    bug fix power data

--- tests/load/test_power_data_gsp.py
+++ tests/load/test_power_data_gsp.py
@@ -8,27 +8,13 @@ from ukpn.load import OpenGSPData, check_for_negative_data, get_gsp_data_in_dict
 
 def test_write_netcdf():
     folder_destination = "tests/data/grafana_dashboard"
-    folder_to_save = "tests/data/grafana_dashboard"
     file_name = "ukpn_gsp.nc"
 
     # check if file exists
-    file_path = os.path.join(folder_to_save, file_name)
+    file_path = os.path.join(folder_destination, file_name)
     check_file = os.path.isfile(file_path)
-    if not check_file:
-        xr_data = OpenGSPData(
-            folder_destination=folder_destination,
-            folder_to_save=folder_to_save,
-            file_name=file_name,
-            write_as_netcdf=True,
-        )
 
-        xr_data = iter(xr_data)
-        if xr_data is not None:
-            print("\nWriting data into NetCDF is successful")
-        else:
-            print("\nWriting data into NetCDF is unsuccessful")
-
-    else:
+    if check_file:
         xr_data = xr.open_dataset(file_path, engine="h5netcdf")
 
     data_coords = list(xr_data.coords)
